e2e55efc4d7d301249faddbabe1b20546069787c
StrandedSoftwareDeveloper <47873522+StrandedSoftwareDeveloper@users.noreply.github.com>
2025-11-25 00:01:12 -0500

    Initial commit

---
+++ AfterBuild.sh
@@ -0,0 +1,2 @@
+cp bin/Debug/net9.0/KSADecapitator.dll "$KSA_DEV_FOLDER/Content/KSADecapitator/"
+cd "$KSA_DEV_FOLDER"
---
+++ KSADecapitator.cs
@@ -0,0 +1,34 @@
+﻿namespace KSADecapitator;
+using StarMap.API;
+using HarmonyLib;
+
+[StarMapMod]
+public class KSADecapitator {
+    [StarMapBeforeMain]
+    public void preMain() {
+        Console.WriteLine("[KSADecapitator]: Hi from before main!");
+        ProgramPatches.Patch();
+    }
+}
+
+[HarmonyPatch]
+public class ProgramPatches {
+    private static Harmony harmony = new Harmony("KSADecapitator");
+
+    public static void Patch() {
+        Console.WriteLine("Patching...");
+        harmony.PatchAll(typeof(ProgramPatches).Assembly);
+    }
+
+    public static void Unload() {
+        harmony.UnpatchAll(harmony?.Id);
+    }
+
+    [HarmonyPatch(typeof(Brutal.GlfwApi.Glfw), nameof(Brutal.GlfwApi.Glfw.Init))]
+    [HarmonyPrefix]
+    public static bool initPatch(ref bool __result) {
+        Console.WriteLine("Hi from GlfwInit");
+        __result = true;
+        return false;
+    }
+}
